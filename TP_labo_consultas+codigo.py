92b9dc26ab5fb255e3d8272aeef2352ea09187bb
Pablo Fernandez <pablofernandezcontacto@gmail.com>
2023-05-13 23:17:28 -0300

    consultas hechas

--- TP_labo_consultas+codigo.py
+++ TP_labo_consultas+codigo.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
+import pandas as pd
+from inline_sql import sql, sql_val
 
-
-padron= pd.read_csv("/home/tsl2004/Escritorio/labo-datos/tablas_originales/padron-de-operadores-organicos-certificados.csv")
+#padron= pd.read_csv("/home/tsl2004/Escritorio/labo-datos/tablas_originales/padron-de-operadores-organicos-certificados.csv")
 #dict_salario = pd.read_csv("")
-localidades_censales =  pd.read_csv("localidades-censales.csv")
-dict_deptos =  pd.read_csv("diccionario_cod_depto.csv")
-dict_act = pd.read_csv("diccionario_clae2.csv")
-padron= pd.read_csv("Padron_en_3raFN.csv")
-provincias =pd.read_csv("tabla_provincia_id_U_provincia.csv")
+localidades_censales =  pd.read_csv("C:\\Users\\pablo\\PycharmProjects\\labo-datos\\tablas_originales_en_1raFN\\loc_sensales_1raFN.csv")
+#dict_deptos =  pd.read_csv("diccionario_cod_depto.csv")
+#dict_act = pd.read_csv("diccionario_clae2.csv")
+padron= pd.read_csv("C:\\Users\\pablo\\PycharmProjects\\labo-datos\\tablas_originales_en_1raFN\\Padron_en_3raFN(falta_rubro).csv")
+provincias =pd.read_csv("C:\\Users\\pablo\\PycharmProjects\\labo-datos\\tablas_creadas\\3ra_FN\padron\\tabla_provincia_id_U_provincia.csv")
+deptos = pd.read_csv("C:\\Users\\pablo\\PycharmProjects\\labo-datos\\tablas_creadas\\3ra_FN\loc_sensales\\tabla_depto_id_U_depto_nombre.csv")
+rubro_clae2 = pd.read_csv("C:\\Users\\pablo\\PycharmProjects\\labo-datos\\tablas_creadas\\1ra_FN\\padron\\rubro_clae2.csv")
+rubros = pd.read_csv("C:\\Users\\pablo\\PycharmProjects\\labo-datos\\tablas_creadas\\1ra_FN\\padron\\rubros_en1FN.csv")
 
-ej1 = sql^consulta_1
 consulta_1 = """
                 SELECT DISTINCT provincias.provincia, count(*) as cant_operadores 
                 FROM padron
@@ -20,11 +23,14 @@ consulta_1 = """
 
 
              """
-print(ej1)
 
-ej2 = sql^consulta_2
-consulta2 = 
-                """
+ej1 = sql^consulta_1
+#print(ej1)
+
+consultaa = '''
+SELECT UPPER(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(localidades_sensales.departamento_nombre,'á','a'),'é','e'),'í','i'),'ó','o'),'ú','u')) as departamento_loc'''
+consulta2 = """"
+            
                 SELECT DISTINCT loc.sensales.departamentos
                 FROM loc sensales
                 EXCEPT(
@@ -33,21 +39,23 @@ consulta2 =
                     )
 
                 """
-                
-consulta3 = 
-                """
-                SELECT padron.rubro, COUNT(*)
-                FROM padron
-                INNER JOIN tabla_productos_en_1FN
-                ON establecimiento = establecimiento and razon_social = razon_social
-                INNER JOIN rubro.rubro_clae2
-                ON rubro_clae_2
-                GROUP BY rubro 
-               """
-ej3 = sql^consulta_3
-            
+ej2 = sql^consulta2
+print(ej2)
 
-tabla_productos = pd.read_csv('/home/tsl2004/Escritorio/labo-datos/tablas_creadas/1ra_FN/padron/tabla_productos_mod.csv')
+consulta3 = """
+                SELECT r.rubro, COUNT(*)
+                FROM rubros r
+                INNER JOIN rubro_clae2 rc ON rc.rubro = r.rubro
+                GROUP BY r.rubro 
+                """
+print(sql^consulta3)
+
+# 4)iv) ¿Cuál fue el salario promedio de esa actividad en 2022? (si hay varios
+#       registros de salario, mostrar el más actual de ese año)
+
+
+
+#tabla_productos = pd.read_csv('/home/tsl2004/Escritorio/labo-datos/tablas_creadas/1ra_FN/padron/tabla_productos_mod.csv')
 
 
 #CONSULTAS
--- consulta.py
+++ consulta.py
@@ -8,21 +8,38 @@ from inline_sql import sql, sql_val
 
 
 if __name__ == "__main__":
+    padron = pd.read_csv(
+        "C:\\Users\\pablo\\PycharmProjects\\labo-datos\\tablas_originales_en_1raFN\\Padron_en_3raFN(falta_rubro).csv")
+    provincias = pd.read_csv(
+        "C:\\Users\\pablo\\PycharmProjects\\labo-datos\\tablas_creadas\\3ra_FN\padron\\tabla_provincia_id_U_provincia.csv")
+    deptos = pd.read_csv(
+        "C:\\Users\\pablo\\PycharmProjects\\labo-datos\\tablas_creadas\\3ra_FN\loc_sensales\\tabla_depto_id_U_depto_nombre.csv")
+    rubro_clae2 = pd.read_csv(
+        "C:\\Users\\pablo\\PycharmProjects\\labo-datos\\tablas_creadas\\1ra_FN\\padron\\rubro_clae2.csv")
+    rubros = pd.read_csv(
+        "C:\\Users\\pablo\\PycharmProjects\\labo-datos\\tablas_creadas\\1ra_FN\\padron\\rubros_en1FN.csv")
+    provincia = pd.read_csv("C:\\Users\\pablo\\PycharmProjects\\labo-datos\\tablas_creadas\\3ra_FN\loc_sensales\\tabla_prov_id_U_prov_nombre1.csv")
+    salario_medio = pd.read_csv("C:\\Users\\pablo\\Downloads\\w_median_depto_priv_clae2.csv")
+    consulta3 = """
+                    SELECT DISTINCT rc.clae2,rc.clae2_desc, COUNT(*)
+                    FROM rubros r
+                    INNER JOIN rubro_clae2 rc ON rc.rubro = r.rubro
+                    GROUP BY rc.clae2_desc,rc.clae2 
+                    """
+    tabla3 = sql ^ consulta3
 
-    padron = pd.read_csv("C:\\Users\\pablo\\PycharmProjects\\labo-datos\\tablas_originales_en_1raFN\\Padron_en_3raFN.csv")
-    municipio_dpto_prov = pd.read_csv("C:\\Users\\pablo\\PycharmProjects\\labo-datos\\tablas_creadas\\3ra_FN\loc_sensales\\tabla_prov_id_U_depto_id_U_muni_id.csv")
-    localidades = pd.read_csv("C:\\Users\\pablo\\PycharmProjects\\labo-datos\\tablas_originales_en_1raFN\\loc_sensales_1raFN.csv")
-    rubros = pd.read_csv("C:\\Users\\pablo\\PycharmProjects\\labo-datos\\tablas_creadas\\1ra_FN\padron\\rubros_en1FN")
-    consulta = sql ^ '''
-                        SELECT p1.provincia_id FROM padron p1 WHERE p1.provincia_id IN
-                        (SELECT p2.provincia_id FROM padron p2 WHERE p1.provincia_id != p2.provincia_id AND p1.localidad = p2.localidad
-                        AND p1.departamento = p2.departamento)
-    
+    actividad = tabla3.iloc[0,0]
+    actividad = str(actividad)
+    consulta4 = '''
+                SELECT AVG(w_median) AS promedio FROM salario_medio WHERE clae2 = $actividad AND fecha = '2022-12-01' AND w_median > 0
+    '''
+
+    print(sql ^ consulta4)
+    con = '''SELECT YEAR(CAST(fecha AS DATE)) as fecha,AVG(w_median) promedio_anual,stddev(w_median) AS desvio_estandar FROM salario_medio GROUP BY YEAR(CAST(fecha AS DATE)) ORDER BY fecha'''
+    print(sql^con)
+    provincial = '''
+                SELECT YEAR(CAST(fecha AS DATE)) as fecha,p.provincia_nombre as provincia,AVG(w_median) AS promedio_anual_provincial,stddev(w_median) AS desvio_estandar FROM salario_medio s INNER JOIN provincia p 
+                ON p.provincia_id = s.id_provincia_indec GROUP BY YEAR(CAST(fecha AS DATE)),p.provincia_nombre ORDER BY YEAR(CAST(fecha AS DATE))
     
     '''
-    consulta_localidad = sql ^ '''SELECT DISTINCT departamento_id,municipio_id FROM localidades'''#1847
-    print(consulta_localidad.head(1847))
-    otra = sql ^ '''
-                SELECT DISTINCT departamento_id,municipio_id FROM municipio_dpto_prov
-    '''
-    #print(otra)
+    print(sql^provincial)
